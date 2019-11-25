dd027d760dfe1974dd7674bd0ce7d7ae6434ea95
carloosbaroon <46849851+carloosbaroon@users.noreply.github.com>
2019-11-25 07:26:12 -0600

    Update main.dart

--- whatsapp_clone/lib/main.dart
+++ whatsapp_clone/lib/main.dart
@@ -1,6 +1,12 @@
 import 'package:flutter/material.dart';
 import 'package:whatsapp_clone/whatsapp_home.dart';
 
-void main() {
 
+void main()=> runApp(new MyApp());
+
+class MyApp extends StatelessWidget {
+  @override
+  Widget build(BuildContext context) {
+
+  }
 }
